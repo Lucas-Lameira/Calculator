f1c17b9dd949f9f0af38aade3c10fb3e146a9f15
Lucas de souza lameira <lucasslameira@gmail.com>
2020-08-21 20:58:54 -0300

    calculadora front-end XD

---
+++ script.js
@@ -0,0 +1,31 @@
+let store = [];
+
+function sla(x) 
+{
+    store.push(x); 
+    console.log(store);   
+}
+
+function soma () 
+{
+
+}
+
+function subtrai () 
+{
+
+}
+
+function divide ()
+{
+
+}
+
+function multiplica () 
+{
+
+}
+
+function igual () {
+
+}
